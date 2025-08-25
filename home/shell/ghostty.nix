5e8c24c159c2702388f6cb7c583b96bad45c7314
Matthew Hrehirchuk <matthew_hre@outlook.com>
2025-08-25 17:55:28 -0600

    feat: clean up ghostty config

--- home/shell/ghostty.nix
+++ home/shell/ghostty.nix
@@ -5,33 +5,6 @@
   ...
 }: let
   ghostty = inputs.ghostty.packages.${pkgs.system}.default;
-  ghosttyTheme = pkgs.writeTextFile {
-    name = "ghostty-theme-Dracula";
-    text = ''
-      palette = 0=#000000
-      palette = 1=#ff5555
-      palette = 2=#50fa7b
-      palette = 3=#f1fa8c
-      palette = 4=#bd93f9
-      palette = 5=#ff79c6
-      palette = 6=#8be9fd
-      palette = 7=#bbbbbb
-      palette = 8=#555555
-      palette = 9=#ff5555
-      palette = 10=#50fa7b
-      palette = 11=#f1fa8c
-      palette = 12=#bd93f9
-      palette = 13=#ff79c6
-      palette = 14=#8be9fd
-      palette = 15=#ffffff
-      background = #282a36
-      foreground = #f8f8f2
-      cursor-color = #f8f8f2
-      cursor-text = #282a36
-      selection-background = #44475a
-      selection-foreground = #ffffff
-    '';
-  };
 in {
   config = {
     home = {
@@ -44,7 +17,6 @@ in {
         mkKeyValue = lib.generators.mkKeyValueDefault {} " = ";
         listsAsDuplicateKeys = true;
       } {
-        background-opacity = 1;
         confirm-close-surface = false;
         copy-on-select = true;
         font-family = "FiraCode Nerd Font";
@@ -54,13 +26,12 @@ in {
 
         shell-integration = "fish";
         window-inherit-working-directory = true;
-        theme = "${ghosttyTheme}";
-        unfocused-split-opacity = 0.9;
+        theme = "Dracula";
         window-decoration = false;
+        window-theme = "ghostty";
         window-padding-x = 16;
         window-padding-y = 16;
         window-padding-balance = true;
-        window-theme = "ghostty";
         keybind = ["ctrl+shift+l=new_split:right" "ctrl+shift+d=new_split:down"];
       };
   };
