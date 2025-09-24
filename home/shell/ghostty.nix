0549b7035fb3108005de8b7c6dc4a962c2372d82
Matthew Hrehirchuk <matthew_hre@outlook.com>
2025-09-24 09:47:14 -0600

    feat: moved to ghostty home manager module

--- home/shell/ghostty.nix
+++ home/shell/ghostty.nix
@@ -1,37 +1,34 @@
 {
-  inputs,
-  lib,
   pkgs,
+  inputs,
   ...
 }: let
   ghostty = inputs.ghostty.packages.${pkgs.system}.default;
 in {
-  config = {
-    home = {
-      packages = [ghostty];
-      sessionVariables = {GHOSTTY_RESOURCES_DIR = "${ghostty}/share";};
+  home.sessionVariables = {
+    GHOSTTY_RESOURCES_DIR = "${ghostty}/share/ghostty";
+  };
+
+  programs.ghostty = {
+    enable = true;
+    package = ghostty;
+    enableFishIntegration = true;
+
+    settings = {
+      confirm-close-surface = false;
+      font-family = "FiraCode Nerd Font";
+      font-size = 11;
+      font-style = "Medium";
+      gtk-wide-tabs = false;
+
+      window-inherit-working-directory = true;
+      theme = "Dracula";
+      window-decoration = true;
+      window-theme = "ghostty";
+      window-padding-x = 16;
+      window-padding-y = 16;
+      window-padding-balance = true;
+      keybind = ["ctrl+shift+l=new_split:right" "ctrl+shift+d=new_split:down"];
     };
-
-    xdg.configFile."ghostty/config".text =
-      lib.generators.toKeyValue {
-        mkKeyValue = lib.generators.mkKeyValueDefault {} " = ";
-        listsAsDuplicateKeys = true;
-      } {
-        confirm-close-surface = false;
-        font-family = "FiraCode Nerd Font";
-        font-size = 11;
-        font-style = "Medium";
-        gtk-wide-tabs = false;
-
-        shell-integration = "fish";
-        window-inherit-working-directory = true;
-        theme = "Dracula";
-        window-decoration = true;
-        window-theme = "ghostty";
-        window-padding-x = 16;
-        window-padding-y = 16;
-        window-padding-balance = true;
-        keybind = ["ctrl+shift+l=new_split:right" "ctrl+shift+d=new_split:down"];
-      };
   };
 }
