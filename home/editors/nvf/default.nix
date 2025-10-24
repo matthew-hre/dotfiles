47532e02a0e09c186264e15ea3176da0eff8ed18
Matthew Hrehirchuk <matthew_hre@outlook.com>
2025-10-24 11:53:00 -0600

    feat: converted nvf to option

--- home/editors/nvf/default.nix
+++ home/editors/nvf/default.nix
@@ -1,66 +1,73 @@
-{inputs, ...}: {
-  imports = [
-    inputs.nvf.homeManagerModules.default
+{
+  config,
+  inputs,
+  lib,
+  ...
+}: {
+  config = lib.mkIf config.users.matthew_hre.configs.nvf {
+    imports = [
+      inputs.nvf.homeManagerModules.default
 
-    ./languages.nix
-  ];
+      ./languages.nix
+    ];
 
-  programs.nvf = {
-    enable = true;
+    programs.nvf = {
+      enable = true;
 
-    settings.vim = {
-      viAlias = true;
-      vimAlias = true;
+      settings.vim = {
+        viAlias = true;
+        vimAlias = true;
 
-      syntaxHighlighting = true;
-      options = {
-        tabstop = 2;
-        shiftwidth = 2;
+        syntaxHighlighting = true;
+        options = {
+          tabstop = 2;
+          shiftwidth = 2;
+        };
+        preventJunkFiles = true;
+
+        clipboard.providers.wl-copy.enable = true;
+
+        keymaps = [
+          {
+            mode = "!";
+            key = "<C-l>";
+            action = "<c-g>u<Esc>[s1z=`]a<c-g>u";
+          }
+        ];
+
+        binds.cheatsheet.enable = true;
+        binds.whichKey.enable = true;
+
+        lsp = {
+          enable = true;
+          formatOnSave = true;
+        };
+
+        theme = {
+          enable = true;
+          name = "dracula";
+        };
+
+        statusline.lualine = {
+          enable = true;
+          globalStatus = true;
+          icons.enable = true;
+        };
+
+        telescope.enable = true;
+        presence.neocord.enable = true;
+
+        autocomplete = {
+          enableSharedCmpSources = true;
+          blink-cmp.enable = true;
+        };
+
+        ui.noice.enable = true;
+
+        ui.borders.plugins.which-key.style = "solid";
+
+        visuals.nvim-web-devicons.enable = true;
       };
-      preventJunkFiles = true;
-
-      clipboard.providers.wl-copy.enable = true;
-
-      keymaps = [
-        {
-          mode = "!";
-          key = "<C-l>";
-          action = "<c-g>u<Esc>[s1z=`]a<c-g>u";
-        }
-      ];
-
-      binds.cheatsheet.enable = true;
-      binds.whichKey.enable = true;
-
-      lsp = {
-        enable = true;
-        formatOnSave = true;
-      };
-
-      theme = {
-        enable = true;
-        name = "dracula";
-      };
-
-      statusline.lualine = {
-        enable = true;
-        globalStatus = true;
-        icons.enable = true;
-      };
-
-      telescope.enable = true;
-      presence.neocord.enable = true;
-
-      autocomplete = {
-        enableSharedCmpSources = true;
-        blink-cmp.enable = true;
-      };
-
-      ui.noice.enable = true;
-
-      ui.borders.plugins.which-key.style = "solid";
-
-      visuals.nvim-web-devicons.enable = true;
     };
   };
 }
