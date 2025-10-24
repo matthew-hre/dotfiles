778a26f9b9ddd5f72ac340b86f0cc31214a115a3
Matthew Hrehirchuk <matthew_hre@outlook.com>
2025-10-24 17:13:45 -0600

    feat: added shell scripts for rebuilding

--- system/shared/packages.nix
+++ system/shared/packages.nix
@@ -4,5 +4,12 @@
     nomacs
     obsidian
     vim
+
+    (pkgs.writeShellScriptBin "rebuild" ''
+      sudo nixos-rebuild switch --flake ~/nix-config#$HOSTNAME 2>&1 | nom
+    '')
+    (pkgs.writeShellScriptBin "rebuild-test" ''
+      sudo nixos-rebuild test --flake ~/nix-config#$HOSTNAME 2>&1 | nom
+    '')
   ];
 }
