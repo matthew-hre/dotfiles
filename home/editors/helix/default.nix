7d451b1166ffdb2b5c09fa4a7503be94ece4d1cd
Matthew Hrehirchuk <matthew_hre@outlook.com>
2025-10-24 11:52:36 -0600

    feat: converted helix to option

--- home/editors/helix/default.nix
+++ home/editors/helix/default.nix
@@ -1,24 +1,30 @@
 {
-  programs.helix = {
-    enable = true;
-    settings = {
-      theme = "dracula";
-      editor = {
-        color-modes = true;
-        cursorline = true;
-        cursor-shape = {
-          insert = "bar";
-          normal = "block";
-          select = "underline";
+  config,
+  lib,
+  ...
+}: {
+  config = lib.mkIf config.users.matthew_hre.configs.helix {
+    programs.helix = {
+      enable = true;
+      settings = {
+        theme = "dracula";
+        editor = {
+          color-modes = true;
+          cursorline = true;
+          cursor-shape = {
+            insert = "bar";
+            normal = "block";
+            select = "underline";
+          };
+          undercurl = true;
+          true-color = true;
+          lsp = {
+            display-messages = true;
+            display-inlay-hints = true;
+          };
+          auto-pairs = true;
+          clipboard-provider = "wayland";
         };
-        undercurl = true;
-        true-color = true;
-        lsp = {
-          display-messages = true;
-          display-inlay-hints = true;
-        };
-        auto-pairs = true;
-        clipboard-provider = "wayland";
       };
     };
   };
