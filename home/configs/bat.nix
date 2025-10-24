4cba2546afbc9dec79dbec8110794301c66bef30
Matthew Hrehirchuk <matthew_hre@outlook.com>
2025-10-24 11:46:31 -0600

    feat: converted bat to option

--- home/configs/bat.nix
+++ home/configs/bat.nix
@@ -1,8 +1,14 @@
 {
-  programs.bat = {
-    enable = true;
-    config = {
-      theme = "Dracula";
+  config,
+  lib,
+  ...
+}: {
+  config = lib.mkIf config.users.matthew_hre.configs.bat {
+    programs.bat = {
+      enable = true;
+      config = {
+        theme = "Dracula";
+      };
     };
   };
 }
