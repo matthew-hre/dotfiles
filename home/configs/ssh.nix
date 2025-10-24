6f96f941a314a08f3cf52014505c689a33a6d58f
Matthew Hrehirchuk <matthew_hre@outlook.com>
2025-10-24 11:51:39 -0600

    feat: converted ssh to option

--- home/configs/ssh.nix
+++ home/configs/ssh.nix
@@ -1,9 +1,16 @@
 {
-  programs.ssh = {
-    enable = true;
-    extraConfig = "
-Host *
-  IdentityAgent ~/.1password/agent.sock
-    ";
-  };
+  config,
+  lib,
+  ...
+}: {
+  config =
+    lib.mkIf config.users.matthew_hre.configs.ssh {
+      programs.ssh = {
+        enable = true;
+        extraConfig = "
+  Host *
+    IdentityAgent ~/.1password/agent.sock
+      ";
+      };
+    };
 }
