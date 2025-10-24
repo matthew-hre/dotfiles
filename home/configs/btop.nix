6aa96927e59506ca1a934881828115f2dd867003
Matthew Hrehirchuk <matthew_hre@outlook.com>
2025-10-24 11:46:55 -0600

    feat: converted btop to option

--- home/configs/btop.nix
+++ home/configs/btop.nix
@@ -3,23 +3,25 @@
   lib,
   ...
 }: {
-  programs.btop = {
-    enable = true;
-    package = pkgs.btop.overrideAttrs (old: rec {
-      buildInputs = (old.buildInputs or []) ++ [pkgs.rocmPackages.rocm-smi];
-      postFixup = lib.concatStringsSep "\n" [
-        (old.postFixup or "")
-        ''
-          patchelf --add-rpath ${lib.getLib pkgs.rocmPackages.rocm-smi}/lib \
-            $out/bin/btop
-        ''
-      ];
-    });
+  config = lib.mkIf config.users.matthew_hre.configs.btop {
+    programs.btop = {
+      enable = true;
+      package = pkgs.btop.overrideAttrs (old: rec {
+        buildInputs = (old.buildInputs or []) ++ [pkgs.rocmPackages.rocm-smi];
+        postFixup = lib.concatStringsSep "\n" [
+          (old.postFixup or "")
+          ''
+            patchelf --add-rpath ${lib.getLib pkgs.rocmPackages.rocm-smi}/lib \
+              $out/bin/btop
+          ''
+        ];
+      });
 
-    settings = {
-      color_theme = "Dracula";
-      theme_background = false;
-      selected_gpus = "0";
+      settings = {
+        color_theme = "Dracula";
+        theme_background = false;
+        selected_gpus = "0";
+      };
     };
   };
 }
