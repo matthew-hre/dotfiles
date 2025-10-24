e8a9a3b56282728d51d5648cb882ec4d233d98b1
Matthew Hrehirchuk <matthew_hre@outlook.com>
2025-10-24 11:47:26 -0600

    feat: converted direnv to option

--- home/configs/direnv.nix
+++ home/configs/direnv.nix
@@ -1,15 +1,17 @@
 {
-  programs.direnv = {
-    enable = true;
-    nix-direnv.enable = true;
-    config = {
-      global = {
-        disable_stdin = true;
-        hide_env_diff = true;
-        warn_timeout = "0ms";
+  config = lib.mkIf config.users.matthew_hre.configs.direnv {
+    programs.direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+      config = {
+        global = {
+          disable_stdin = true;
+          hide_env_diff = true;
+          warn_timeout = "0ms";
+        };
       };
     };
-  };
 
-  programs.git.ignores = [".direnv/"];
+    programs.git.ignores = [".direnv/"];
+  };
 }
